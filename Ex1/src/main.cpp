ff472dabf40b7116cb913e6e07484303e5d197f6
Woodley Gelin <72886722+kinglaine@users.noreply.github.com>
2022-09-07 16:22:39 -0400

    Add Ex1 folder

---
+++ Ex1/src/main.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream> //preprocessor directive. iostream is the input output class in C++
+// main entry point
+int main() {
+// write a message to console
+std::cout << "Hello world!\n";
+return 0;
+}
+
+
