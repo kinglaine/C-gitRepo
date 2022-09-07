7a67c86b98ff0257add3a198f8eba6c1fbc78f66
Woodley Gelin <72886722+kinglaine@users.noreply.github.com>
2022-09-07 17:30:31 -0400

    Update main.cpp

--- Ex1/src/main.cpp
+++ Ex1/src/main.cpp
@@ -1,10 +1,7 @@
 // Copyright 2022 Woodley Gelin
 #include <iostream> 
 // main entry point
-int main() {
-// write a message to console
+int main() { // write a message to console
   std::cout << "Hello world!\n";
   return 0;
 }
-
-
