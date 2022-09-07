ad801ad21b396ad7ff16efb10745e3737df18d54
Woodley Gelin <72886722+kinglaine@users.noreply.github.com>
2022-09-07 17:20:12 -0400

    Update main.cpp

--- Ex1/src/main.cpp
+++ Ex1/src/main.cpp
@@ -1,12 +1,12 @@
+//Copyright 2022 Woodley Gelin
+
 #include <iostream> 
 
-//preprocessor directive.
 // main entry point
 
 int main() {
 // write a message to console
-
-std::cout << "Hello world!\n";
+  std::cout << "Hello world!\n";
   return 0;
 }
 
