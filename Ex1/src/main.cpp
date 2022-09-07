bb45a88f7b2ef642c34a6d5901e186adc302f55a
Woodley Gelin <72886722+kinglaine@users.noreply.github.com>
2022-09-07 17:52:52 -0400

    Update main.cpp

--- Ex1/src/main.cpp
+++ Ex1/src/main.cpp
@@ -2,12 +2,11 @@
 #include <iostream>  // preprocessor directive. iostream is the input output class in C++
 // main entry point
 int main() {
-  
-// write a message to console
-  
-std::cout << "Hello world!\n";
-  
-return 0;
-  
-}
 
+// write a message to console
+
+std::cout << "Hello world!\n";
+
+return 0;
+
+}
